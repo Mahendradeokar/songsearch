02733bde1d0d9278a1a8ef7108dc172681af5ceb
Mahendradeokar <deokarmahendra424@gmail.com>
2025-09-08 19:56:42 +0530

    Chore: Deployment related

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 
 const mode = process.env.NODE_ENV || "development";
-console.log("MODE", mode, process.env.NODE_ENV);
-const envFiles = [`.env.${mode}`, ".env"];
+
+// - /etc/secrets/.env.${mode} for production deployment only
+const envFiles = ["/etc/secrets/.env.${mode}", `.env.${mode}`, ".env"];
 dotenv.config({ path: envFiles });
 
 console.log(
